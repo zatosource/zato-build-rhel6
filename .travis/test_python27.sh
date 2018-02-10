fe96f3d6c478020a4fb05d3ef1eb4c69a93265af
David Wilson <dw@botanicus.net>
2018-02-10 13:52:40 +0545

    Fix ACL

--- .travis/test_python27.sh
+++ .travis/test_python27.sh
@@ -5,4 +5,4 @@ sudo apt-get -y install awscli
 
 source $TRAVIS_BUILD_DIR/.travis/setup.sh
 run $TRAVIS_BUILD_DIR/build_python27.sh
-aws s3 cp $TRAVIS_BUILD_DIR/python27.tar.bz2 s3://$ASSETS_BUCKET/python27/python27.tar.bz2
+aws s3 cp --acl=public-read $TRAVIS_BUILD_DIR/python27.tar.bz2 s3://$ASSETS_BUCKET/python27/python27.tar.bz2
