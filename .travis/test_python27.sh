b8eb17e39e485db311a20f87e2f5142502edadab
David Wilson <dw@botanicus.net>
2018-02-10 13:34:54 +0545

    Install awscli.

--- .travis/test_python27.sh
+++ .travis/test_python27.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -ex
+yum -y install awscli
+
 source $TRAVIS_BUILD_DIR/.travis/setup.sh
 run $TRAVIS_BUILD_DIR/build_python27.sh
-aws s3 cp $TRAVIS_BUILD_DIR/python27.tar.bz2 s3://$ASSETS_BUCKET/python27.tar.bz2
+aws s3 cp $TRAVIS_BUILD_DIR/python27.tar.bz2 s3://$ASSETS_BUCKET/python27/python27.tar.bz2
